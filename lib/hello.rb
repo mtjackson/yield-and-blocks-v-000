82f630658657c132dfd68ffd5cfadb93562baf97
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-26 16:03:35 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,11 +2,6 @@ def hello_t(array)
   i = 0
   while i < array.length
     yield array[i]
-    array do |name|
-      if name.start_with?("T")
-        puts "Hi, #{name}"
-      end
-    end
     i += 1
   end
 
